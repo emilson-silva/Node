12818f80614d3a59b4eb958b57d68c5d7180c713
Emilson Silva <emilsongyn17@gmail.com>
2022-12-14 13:41:15 -0300

    Trabalhando com a ferramenta insommia

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-
-
 const express = require('express');
 
 const server = express();
 
 // Query params = ?nome=NodeJS
 // Route Params = /curso/2
+// Request Body = {nome: 'Nodejs', tipo: 'Backend'}
+
+const cursos = [' Node JS', 'JavaScript','React Native'];
+
 
 // localhost:300/curso/2
-
 server.get('/curso/:id', (req, res) => { 
     const id = req.params.id;
     return res.json({curso: `Curso ${id}`});
