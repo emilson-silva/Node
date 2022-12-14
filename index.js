cb64f2b312a5b1bf8e39b63155db2a34108930f7
Emilson Silva <emilsongyn17@gmail.com>
2022-12-14 01:08:10 -0300

    Entendendo query e route params

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 
 const server = express();
 
+// Query params = ?nome=NodeJS
+// Route Params = /curso/2
+
 // localhost:300/curso
 server.get('/curso', (req, res) => { 
     return res.json({curso: 'Node Js'});
